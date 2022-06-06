96e071c7ed6d09a4e9c98b5a5ad5e1ed5632b63e
Iverick <iverick091@gmail.com>
2022-06-06 17:16:46 +0300

    API call to users endpoint.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import './App.css';
-import Navbar from './features/Navbar'
-import Users from './features/users/Users'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Users from './components/users/Users'
 
-function App() {
+const API_URL = "http://localhost:3000/api/v01/"
+
+function getUsersAPIData() {
+  return axios.get(API_URL + "users").then((res) => res.data)
+}
+
+export default function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    // Connecting to API /users endpoint after the element was mounted
+    let mounted = true;
+    getUsersAPIData().then((items) => {
+      if (mounted) {
+        setUsers(items)
+      }
+    })
+    return () => (mounted = false)
+  }, [])
+
   return (
-    <div className="pt-3 px-5">
+    <div className="pt-3 px-5 bg-light bg-gradient">
       <header>
         <Navbar />
       </header>
 
-      <div className="main container px-4 py-5 bg-light bg-gradient">
-        <Users />
+      <div className="main container px-4 py-5">
+        <Users users={users} />
       </div>
     </div>
   );
 }
-
-export default App
