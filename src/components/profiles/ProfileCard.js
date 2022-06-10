77487551cae8b13fdcbb7732139cc0ad582d3b49
Iverick <iverick091@gmail.com>
2022-06-10 17:25:51 +0300

    Refactored ProfileCard to use component state

--- src/components/profiles/ProfileCard.js
+++ src/components/profiles/ProfileCard.js
@@ -1,8 +1,31 @@
-export default function ProfileCard(props) {
-  // Changing date format
-  const date = props.profile.birthday
+import { useEffect, useState } from "react"
+
+// Changes input date format
+const changeInputBirthdayFormat = (date) => {
   const [year, month, day] = date.split('-')
-  const birthday = [day, month, year].join('.');
+  const birthday = [day, month, year].join('.')
+  return birthday
+}
+
+export default function ProfileCard(props) {
+
+  // States for form fields
+  const [userId, setUserId] = useState('')
+  const [profileId, setProfileId] = useState('')
+  const [profileName, setProfileName] = useState('')
+  const [profileGender, setProfileGender] = useState('')
+  const [profileBirthday, setProfileBirthday] = useState('')
+  const [profileCity, setProfileCity] = useState('')
+
+  useEffect(() => {
+    // Sets initial states based on props
+    setUserId(props.profile.user_id)
+    setProfileId(props.profile.id)
+    setProfileName(props.profile.name)
+    setProfileGender(props.profile.gender)
+    setProfileBirthday(changeInputBirthdayFormat(props.profile.birthday))
+    setProfileCity(props.profile.city)
+  }, [])
 
   return (
     <div className="col-3">
@@ -11,10 +34,10 @@ export default function ProfileCard(props) {
         {/* Card body */}
         <div className="card-body">
           <ul className="list-unstyled mt-1 mb-2 text-center">
-            <li className="pb-2 fs-4">{ props.profile.name }</li>
-            <li className="py-2">{ props.profile.gender }</li>
-            <li className="py-2">{ birthday }</li>
-            <li className="pt-2">{ props.profile.city }</li>
+            <li className="pb-2 fs-4">{ profileName }</li>
+            <li className="py-2">{ profileGender }</li>
+            <li className="py-2">{ profileBirthday }</li>
+            <li className="pt-2">{ profileCity }</li>
           </ul>
         </div> 
         
