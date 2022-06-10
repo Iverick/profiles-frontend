b7b4d7a78214aff5a097159be459ea6ab6bfce1e
Iverick <iverick091@gmail.com>
2022-06-10 17:10:24 +0300

    Added footer buttons markup for ProfileCard.

--- src/components/profiles/ProfileCard.js
+++ src/components/profiles/ProfileCard.js
@@ -6,7 +6,9 @@ export default function ProfileCard(props) {
 
   return (
     <div className="col-3">
-      <div className="card mb-5 rounded-3 shadow-sm mx-3">
+      <div className="card mb-5 rounded-5 shadow-sm mx-3">
+
+        {/* Card body */}
         <div className="card-body">
           <ul className="list-unstyled mt-1 mb-2 text-center">
             <li className="pb-2 fs-4">{ props.profile.name }</li>
@@ -14,7 +16,24 @@ export default function ProfileCard(props) {
             <li className="py-2">{ birthday }</li>
             <li className="pt-2">{ props.profile.city }</li>
           </ul>
-        </div>   
+        </div> 
+        
+        {/* Footer buttons */}
+        <div className="d-flex">
+          <div className="text-center border flex-fill">
+            <button className="btn btn-sm btn-white w-100 p-2">
+              <span className="text-muted me-2">edit</span>
+              <i className="fa-solid fa-pencil"></i>
+            </button>
+          </div>
+          <div className="text-center border flex-fill">
+            <button className="btn btn-sm btn-white w-100 p-2">
+              <span className="text-muted me-2">delete</span>
+              <i className="fa-solid fa-trash-can"></i>
+            </button>
+          </div>
+        </div>
+ 
       </div>
     </div>    
   )
