222e8c88c553232a2a8d16753d5d34723094b900
Iverick <iverick091@gmail.com>
2022-06-12 19:01:55 +0300

    Added remote icon image to Navbar.

--- src/components/profiles/AddProfileIconCard.js
+++ src/components/profiles/AddProfileIconCard.js
@@ -6,7 +6,7 @@ export default function AddProfileIconCard() {
         <div className="card-body text-center">
             <button type="button" className="btn btn-white w-100" data-bs-toggle="modal" data-bs-target="#profileBackdrop">
               <div className="d-flex flex-column my-5 py-3">
-                <i class="fa-regular fa-plus fs-1 text-muted"></i>
+                <i className="fa-regular fa-plus fs-1 text-muted"></i>
                 <span className="text-muted">Create new profile</span>
               </div>
             </button>
