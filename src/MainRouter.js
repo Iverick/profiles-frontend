710fed9b22605c2dc5c47b05fa545765aade814a
Iverick <iverick091@gmail.com>
2022-06-14 12:00:34 +0300

    Added redirections in MainRouter.

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import Dashboard from './components/dashboard/Dashboard'
 import Profiles from './components/profiles/Profiles'
 import SignIn from './components/session/SignIn'
@@ -8,17 +9,27 @@ import User from './components/users/User'
 import Users from './components/users/Users'
 
 const MainRouter = () => {
+
+  const { loggedIn } = useSelector((state) => state.auth)
+
   return (
     <div>
+    { loggedIn ? (
       <Routes>
-        <Route path="*">404 not found!</Route>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="*" element= { <Navigate from="*" to="/" exact /> } />
         <Route path="/" element={<Dashboard />} />
         <Route path="/users" element= {<Users />} />
         <Route path="/users/:userId" element={<User />} />
         <Route path="/profiles" element={<Profiles />} />
       </Routes>
+    ) : (
+      <Routes>
+        <Route path="*" element= { <Navigate from="/" to="/signin" exact /> } />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+      )
+    }
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,28 +31,33 @@ export default function Navbar() {
   }
 
   return (
-    <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
-      <a href="/" className="d-flex align-items-center text-dark text-decoration-none ms-3">
-        {/* Userpick image */}
-        <img 
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeeUl9IZDN97pBQNgeunx6dD1df-4g7vkPFw&usqp=CAU" 
-          alt="" 
-          className={ userpickImgClasses.join(" ") } 
-          width="45" 
-          height="45" />
-        <span className="fs-4">{ user ? user.username : 'username' }</span>
-      </a>
+    <div>
+    { loggedIn && (
+      <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+        
+        <a href="/" className="d-flex align-items-center text-dark text-decoration-none ms-3">
+          {/* Userpick image */}
+          <img 
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeeUl9IZDN97pBQNgeunx6dD1df-4g7vkPFw&usqp=CAU" 
+            alt="" 
+            className={ userpickImgClasses.join(" ") } 
+            width="45" 
+            height="45" />
+          <span className="fs-4">{ user.username }</span>
+        </a>
 
-      <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-        <Link className="me-3 py-2 text-dark text-decoration-none" to="/">Dashboard</Link>
-        <Link className="me-3 py-2 text-dark text-decoration-none" to="/profiles">Profiles</Link>
-        <Link className="me-3 py-2 text-dark text-decoration-none" to="/users">Users</Link>
-        <div>
-          <button onClick={ handleLogout } type="button" className="btn ms-3 py-2 btn-outline-muted text-decoration-none">
-            <span className="text-sm">Log out</span>
-          </button>
-        </div>
-      </nav>
+        <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+          <Link className="me-3 py-2 text-dark text-decoration-none" to="/">Dashboard</Link>
+          <Link className="me-3 py-2 text-dark text-decoration-none" to="/profiles">Profiles</Link>
+          <Link className="me-3 py-2 text-dark text-decoration-none" to="/users">Users</Link>
+          <div>
+            <button onClick={ handleLogout } type="button" className="btn ms-3 py-2 btn-outline-muted text-decoration-none">
+              <span className="text-sm">Log out</span>
+            </button>
+          </div>
+        </nav>
+      </div>
+    )}
     </div>
   );
 }
