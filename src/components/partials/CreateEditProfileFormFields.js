5153bff421b7f2b64aed6c7513231a5bd28f8275
Iverick <iverick091@gmail.com>
2022-06-15 11:35:19 +0300

    Refactored CreateProfileModal to move fields into the partial.

---
+++ src/components/partials/CreateEditProfileFormFields.js
@@ -0,0 +1,87 @@
+// Return today's date
+const setToday = () => {
+  return new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().split("T")[0]
+}
+
+export default function CreateEditProfileFormFields(props) {
+  const { handleChange } = props
+
+  return (
+    <div>
+      {/* name field */}
+      <div className="form-floating my-4">
+        <input
+          type="text"
+          name="name"
+          className="form-control border-0"
+          id="name-field"
+          value={props.values.name}
+          onChange={handleChange} />
+        <label htmlFor="name-field">Name</label>
+      </div>
+
+      {/* birthday field */}
+      <div className="form-floating my-4">
+        <input
+          type="date"
+          name="birthday"
+          className="form-control border-0"
+          id="birthday-field"
+          max={setToday()}
+          value={props.values.birthday}
+          onChange={handleChange} />
+        <label htmlFor="birthday-field">Birthday</label>
+      </div>
+
+      {/* radio buttons */}
+      <div className="form-floating my-4">
+        <div className="row justify-content-evenly">
+          <small className="text-muted ms-4 mb-3">Gender</small>
+          <div className="col-4">
+            <div className="form-check form-check-inline">
+              <label className="form-check-label" htmlFor="male-radio">
+                <span className="text-secondary">Male</span>
+              </label>
+              <input
+                className="form-check-input flex-shrink-0"
+                type="radio"
+                name="gender"
+                id="male-radio"
+                value="male"
+                checked={props.values.gender === 'male'}
+                onChange={handleChange} />
+            </div>
+          </div>
+
+          <div className="col-4">
+            <div className="form-check form-check-inline ms-4">
+              <label className="form-check-label" htmlFor="female-radio">
+                <span className="text-secondary">Female</span>
+              </label>
+              <input
+                className="form-check-input flex-shrink-0"
+                type="radio"
+                name="gender"
+                id="female-radio"
+                value="female"
+                checked={props.values.gender === 'female'}
+                onChange={handleChange} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* city field */}
+      <div className="form-floating my-4">
+        <input
+          type="text"
+          name="city"
+          className="form-control border-0"
+          id="city-field"
+          value={props.values.city}
+          onChange={handleChange} />
+        <label htmlFor="city-field">City</label>
+      </div>
+    </div>
+  )
+}
--- src/components/profiles/CreateProfileModal.js
+++ src/components/profiles/CreateProfileModal.js
@@ -1,7 +1,8 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
 import { useSelector } from 'react-redux'
-import { postProfileAPIData } from'../../services/profile.service'
+import { postProfileAPIData } from '../../services/profile.service'
 import SubmitRejectProfileButtons from '../partials/SubmitRejectProfileButtons'
+import CreateEditProfileFormFields from '../partials/CreateEditProfileFormFields'
 
 // Return today's date
 const setToday = () => {
@@ -44,14 +45,16 @@ export default function CreateProfileModal(props) {
       ...state,
       [name]: value
     })
+    console.log(state)
   }
 
   const handleSubmit = (e) => {
-    console.log(state)
     e.preventDefault()
     if (state.name === '' || state.birthday === '' || state.gender === '' || state.city === '' || userId === null) {
       setError(true)
     } else {
+      setError(false)
+      setSuccess(false)
       const createdData = state
       createdData['user_id'] = userId
       postProfileAPIData(createdData).then((res) => {
@@ -72,93 +75,19 @@ export default function CreateProfileModal(props) {
         <div className="modal-content">
           <div className="modal-body">
             <div className="px-5">
-            {/* TODO Create the form to handle Profile POST and PUT requests */}
 
-            <div className="text-success" style={{ display: success ? '' : 'none'}}>
-              Profile was created!
-            </div>
-
-            <div className="text-danger" style={{ display: error ? '' : 'none'}}>
-              There were errors creating a profile
-            </div>
-
-            {/* name field */}
-            <div className="form-floating my-4">
-              <input
-                type="text"
-                name="name"
-                className="form-control border-0"
-                id="name-field"
-                value={ state.name }
-                onChange={ handleChange } />
-              <label htmlFor="name-field">Name</label>
-            </div>
-            
-            {/* birthday field */}
-            <div className="form-floating my-4">
-              <input
-                type="date"
-                name="birthday"
-                className="form-control border-0"
-                id="birthday-field"
-                max={ setToday() }
-                value={ state.birthday }
-                onChange={ handleChange } />
-              <label htmlFor="birthday-field">Birthday</label>
-            </div>
-
-            {/* radio buttons */}
-            <div className="form-floating my-4">
-            <div className="row justify-content-evenly">
-              <small className="text-muted ms-4 mb-3">Gender</small>
-              <div className="col-4">
-                <div className="form-check form-check-inline">
-                  <label className="form-check-label" htmlFor="male-radio">
-                    <span className="text-secondary">Male</span>
-                  </label>
-                  <input
-                    className="form-check-input flex-shrink-0"
-                    type="radio"
-                    name="gender"
-                    id="male-radio"
-                    value="male"
-                    checked={ state.gender === 'male' }
-                    onChange={ handleChange } />
-                </div>
+              <div className="text-success" style={{ display: success ? '' : 'none' }}>
+                Profile was created!
               </div>
 
-              <div className="col-4">
-                <div className="form-check form-check-inline ms-4">
-                  <label className="form-check-label" htmlFor="female-radio">
-                    <span className="text-secondary">Female</span>
-                  </label>
-                  <input
-                    className="form-check-input flex-shrink-0"
-                    type="radio"
-                    name="gender"
-                    id="female-radio"
-                    value="female"
-                    checked={ state.gender === 'female' }
-                    onChange={ handleChange } />
-                </div>
+              <div className="text-danger" style={{ display: error ? '' : 'none' }}>
+                There were errors creating a profile
               </div>
-            </div>
-            </div>
 
-            {/* city field */}
-            <div className="form-floating my-4">
-              <input
-                type="text"
-                name="city"
-                className="form-control border-0"
-                id="city-field"
-                value={ state.city }
-                onChange={ handleChange } />
-              <label htmlFor="city-field">City</label>
-            </div>
+              <CreateEditProfileFormFields values={state} handleChange={handleChange} />
 
-            {/* submit/reject buttons */}
-            <SubmitRejectProfileButtons handleSubmit={ handleSubmit } />
+              {/* submit/reject buttons */}
+              <SubmitRejectProfileButtons handleSubmit={handleSubmit} />
             </div>
 
           </div>
